d7b39eb21eac778f761e6d4558410ac6a7459ac5
Koji Wakamiya <koji.wakamiya@gmail.com>
2022-06-07 20:25:21 +0900

    Add CacheStrategy

--- lib/src/http_hive_cache.dart
+++ lib/src/http_hive_cache.dart
@@ -4,6 +4,12 @@ import 'package:http_hive_cache/src/adapter.dart';
 import 'package:http_hive_cache/src/hive_helper.dart';
 import 'package:http_hive_cache/src/http_cache.dart';
 
+enum CacheStrategy {
+  noCache,
+  client,
+  server,
+}
+
 class HttpHiveCache {
   static Future<void> init({
     String subDir = 'http_cache',
@@ -29,9 +35,17 @@ class HttpHiveCache {
   static Future<http.Response> get(
     Uri url, {
     Map<String, String>? headers,
+    CacheStrategy strategy = CacheStrategy.client,
     Duration custom = const Duration(days: 365),
     bool forceRefresh = false,
   }) async {
+    if (strategy == CacheStrategy.noCache) {
+      return await http.get(
+        url,
+        headers: headers,
+      );
+    }
+
     final now = DateTime.now();
 
     final key = url.toString();
@@ -54,17 +68,63 @@ class HttpHiveCache {
     );
 
     if (response.statusCode >= 200 && response.statusCode < 300) {
-      final until = now.add(custom);
+      switch (strategy) {
+        case CacheStrategy.noCache:
+          throw Exception();
+        case CacheStrategy.client:
+          final cache = HttpCache(
+            url: key,
+            statusCode: response.statusCode,
+            body: response.bodyBytes,
+            headers: response.headers,
+            until: now.add(custom),
+          );
 
-      final cache = HttpCache(
-        url: key,
-        statusCode: response.statusCode,
-        body: response.bodyBytes,
-        headers: response.headers,
-        until: until,
-      );
+          _box.put(key, cache);
+          break;
+        case CacheStrategy.server:
+          final headers = response.headers.map(
+            (key, value) => MapEntry(key.toLowerCase(), value),
+          );
 
-      _box.put(key, cache);
+          if (headers.containsKey('cache-control')) {
+            final header = response.headers['cache-control']!;
+
+            final param = header
+                .split(',')
+                .map((element) => element.trim().toLowerCase())
+                .firstWhere(
+                  (element) =>
+                      element.contains('max-age') ||
+                      element.contains('s-maxage'),
+                  orElse: () => '',
+                );
+            if (param.isNotEmpty) {
+              final age = param.split('=');
+              final cacheSeconds = int.tryParse(age[1]) ?? 0;
+
+              if (cacheSeconds > 0) {
+                final cache = HttpCache(
+                  url: key,
+                  statusCode: response.statusCode,
+                  body: response.bodyBytes,
+                  headers: response.headers,
+                  until: now.add(
+                    Duration(
+                      seconds: cacheSeconds,
+                    ),
+                  ),
+                );
+
+                _box.put(key, cache);
+                break;
+              }
+            }
+          } else {
+            // no cache
+          }
+          break;
+      }
     }
 
     return response;
